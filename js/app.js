2ba28ef28310b3c3180eba7ca01b69c8e425255d
Jose Raul Alfaro Guillen <jralfarog@gmail.com>
2018-02-07 11:48:56 -0600

    fcfdxs

--- js/app.js
+++ js/app.js
@@ -23,13 +23,22 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 })
 
+
+ .constant("serverConfig", {
+        //"url": "http://localhost:80",
+        "url": "https://",
+        "imageStorageURL" : 'https:'
+        //"port": "80"
+    })
+
+
 .config(function($stateProvider, $urlRouterProvider, $httpProvider, $ionicConfigProvider) {
 
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
-
+ $ionicConfigProvider.tabs.position('bottom'); 
     $httpProvider.defaults.headers.common = {};
   $httpProvider.defaults.headers.post = {};
   $httpProvider.defaults.headers.put = {};
